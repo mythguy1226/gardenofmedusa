958bda2e991b2647f453543afea174effc527a7b
mythguy1226 <rdr8959@rit.edu>
2023-10-18 20:33:32 -0400

    Added comments for new async task handling

--- GardenOfMedusa/Source/GardenOfMedusa/AsyncActionNode.cpp
+++ GardenOfMedusa/Source/GardenOfMedusa/AsyncActionNode.cpp
@@ -3,30 +3,37 @@
 
 #include "AsyncActionNode.h"
 
+// Callable node usable in blueprints
 UAsyncActionNode* UAsyncActionNode::MultiThreadedNode()
 {
+	// Return object as async action node
 	UAsyncActionNode* Node = NewObject<UAsyncActionNode>();
 	return Node;
 }
 
+// Activation
 void UAsyncActionNode::Activate()
 {
+	// Creates a Non-abandonable task and starts it in the background
 	(new FAutoDeleteAsyncTask<BP_NonAbandonableTask>(this))->StartBackgroundTask();
 }
 
 //**************************************************************
 
+// Constructor used for setting the calling object
 BP_NonAbandonableTask::BP_NonAbandonableTask(UAsyncActionNode* BP_TaskInstance)
 {
 	CallingObject = BP_TaskInstance;
 }
 
+// Destructor used for clean-up and broadcasting finished work
 BP_NonAbandonableTask::~BP_NonAbandonableTask()
 {
 	CallingObject->FinishedWork.Broadcast();
 	CallingObject->SetReadyToDestroy();
 }
 
+// Method called for broadcasting multi-threaded work
 void BP_NonAbandonableTask::DoWork()
 {
 	CallingObject->MultiThreadedWork.Broadcast();
--- GardenOfMedusa/Source/GardenOfMedusa/AsyncActionNode.h
+++ GardenOfMedusa/Source/GardenOfMedusa/AsyncActionNode.h
@@ -17,15 +17,18 @@ class GARDENOFMEDUSA_API UAsyncActionNode : public UBlueprintAsyncActionBase
 	GENERATED_BODY()
 	
 public:
+	// Function calls for both multi-threaded work and when that work is finished
 	UPROPERTY(BlueprintAssignable)
 	FTaskOutput MultiThreadedWork;
 
 	UPROPERTY(BlueprintAssignable)
 	FTaskOutput FinishedWork;
 
+	// Callable blueprint node
 	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
 	static UAsyncActionNode* MultiThreadedNode();
 
+	// Method called upon activation
 	virtual void Activate() override;
 };
 
@@ -34,16 +37,20 @@ public:
 class BP_NonAbandonableTask : public FNonAbandonableTask
 {
 public:
+	// Constructor/Destructor
 	BP_NonAbandonableTask(UAsyncActionNode* BP_TaskInstance);
 
 	~BP_NonAbandonableTask();
 
+	// Needed for UE5
 	FORCEINLINE TStatId GetStatId() const
 	{
 		RETURN_QUICK_DECLARE_CYCLE_STAT(BP_NonAbandonableTask, STATGROUP_ThreadPoolAsyncTasks);
 	}
 
+	// Object to run work on
 	UAsyncActionNode* CallingObject = nullptr;
 
+	// Method for running multi-threaded work
 	void DoWork();
 };
